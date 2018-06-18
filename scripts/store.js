412c1bf22b4ccf487062b437d4685e85af2d3039
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-18 11:35:58 -0400

    added use strict to js files and created store.js for exercise 1

---
+++ scripts/store.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const store = (function () {
+
+  return {
+    items: [
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'bread', checked: false }
+    ],
+
+    hideCheckedItems: false,
+    searchTerm: ''
+  };
+
+}() );
